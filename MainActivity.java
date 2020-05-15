fca6d4a16fbe7e100946f6dd3255ae3ab3bc8137
TNungo <45259899+TNungo@users.noreply.github.com>
2020-05-15 12:19:51 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,50 +1,58 @@
-package com.muigai.aar;
+package com.nungo.doctorappointment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-      public static final String LOG_TAG = MainActivity.class.getSimpleName();
+import java.util.Calendar;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText date;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        date=findViewById(R.id.date);
+        date.setOnClickListener(this);
+
 
     }
 
+    @Override
+    public void onClick(View v) {
 
+        if (v==date){
 
+            final Calendar c = Calendar.getInstance();
+            mYear=c.get(Calendar.YEAR);
+            mMonth=c.get(Calendar.MONTH);
+            mDay=c.get(Calendar.DAY_OF_MONTH);
 
-    public void emergency(View view) {
+            DatePickerDialog datePickerDialog= new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                   date.setText(dayOfMonth+"-"+(month+1)+"-"+year);
+                }
+            },mYear,mMonth,mDay);
+
+            datePickerDialog.show();
+        }
+
     }
 
-    public void launchRegister(View view) {
-        Intent myIntent= new Intent(this,Register.class);
-        //start a second activity using the method start activity
-        Log.d(LOG_TAG,  "Button Register Clicked");
-
-
-
-        startActivity(myIntent);
-    }
-
-
-    public void launchLogin(View view) {
-        Intent myIntent= new Intent(this,Login.class);
-        //start a second activity using the method start activity
-        Log.d(LOG_TAG,  "Button Login Clicked");
-
-
-
-        startActivity(myIntent);
-
+    public void makeAppointment(View view) {
+        Toast toastEnter= Toast.makeText(this,"Appointment Made Successfully",Toast.LENGTH_SHORT);
+        toastEnter.show();
     }
 }
